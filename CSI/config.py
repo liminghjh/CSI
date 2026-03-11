07053d49b4d406ca7fc780260fa0fe83d9a2bd8e
liming <3059352128@qq.com>
2026-03-12 01:01:38 +0800

    02

--- CSI/config.py
+++ CSI/config.py
@@ -4,7 +4,7 @@
 
 
 #默认大语言模型信息
-api_key="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"                    # 你的API key
+api_key=""                    # 你的API key
 api_url="https://integrate.api.nvidia.com/v1"                   # API地址
 api_model="openai/gpt-oss-120b"                     # 模型名称 
 api_provider="openai"                     # 模型提供商
